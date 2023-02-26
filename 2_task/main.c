6a6ef8ee681fc43ca2c62a4b56646fec0d274c05
gubanovpm <petrgubanov37@gmail.com>
2023-02-26 23:19:23 +0300

    another commit with segfault, sry me from the future

--- 2_task/main.c
+++ 2_task/main.c
@@ -26,50 +26,60 @@ int main(int argc, char *argv[]) {
 	//gmp_printf("lb = %lu | rb = %lu | rank is %d | sum is = %Zd | prod is = %Zd\n", LBMASK(borders), RBMASK(borders), rank, sum, prod);
 	// now we want to send multipl to other process
 	
-	const uint32_t buf_size = 100000;
 	mpz_t fact; mpz_init(fact);
-	char *buf;
 
 	if (rank != 0) {
-		size_t len = 
-		char buf[buf_size]; int
-		for (uint32_t j = 0; j < buf_size; ++j) buf[j] = '\0';
+		size_t len = mpz_sizeinbase(prod, 10);
+		char *buf; MPI_Alloc_mem(len+1, MPI_INFO_NULL, buf);
+		if (!buf) MPI_Abort(MPI_COMM_WORLD, 911);
+
 		len = gmp_sprintf(buf, "%Zd", prod);
+		printf("%s\n", buf);
 		for (int i = rank - 1; i >= 0; --i) {
 			MPI_Send(&len,   1,  MPI_INT, i, 0, MPI_COMM_WORLD);
 			MPI_Send( buf, len, MPI_CHAR, i, 0, MPI_COMM_WORLD);
 		}
+		MPI_Free_mem(buf);
+
 	} else {
 		mpz_add(fact, fact, prod);
 	}
 	for (int i = rank + 1; i < comsize; ++i) {
-		char buf[buf_size]; int len = 0;
-		for (uint32_t j = 0; j < buf_size; ++j) buf[j] = '\0';
-		MPI_Status status;
+		int len = 0; MPI_Status status;
 		MPI_Recv(&len,   1,  MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+		printf("len = %d\n", len);
+		char *buf; MPI_Alloc_mem(len+1, MPI_INFO_NULL, buf);
+		if (!buf) MPI_Abort(MPI_COMM_WORLD, 911);
 		MPI_Recv( buf, len, MPI_CHAR, i, 0, MPI_COMM_WORLD, &status);
+		printf("Recieve number is %s\n", buf);
 		gmp_sscanf(buf, "%Zd", &prod);
 		mpz_mul(sum, sum, prod);
 		if (rank == 0) mpz_mul(fact, fact, prod);
+		MPI_Free_mem(buf);
 	}
 	//gmp_printf("My rank = %d | sum = %Zd\n", rank, sum);
 	MPI_Barrier(MPI_COMM_WORLD);
+
+	char *buf; 
 	if (rank != 0) {
-		char buf[buf_size] ; int len = 0;
-		for (uint32_t j = 0; j < buf_size; ++j) buf[j] = '\0';
+		size_t len = mpz_sizeinbase(sum, 10);
+		MPI_Alloc_mem(len + 1, MPI_INFO_NULL, buf);
+		if (!buf) MPI_Abort(MPI_COMM_WORLD, 911);
+
 		len = gmp_sprintf(buf, "%Zd", sum);
 		//gmp_printf("My rank %d and I send %Zd\n", rank, sum);
 		MPI_Send(&len,   1,  MPI_INT, 0, 0, MPI_COMM_WORLD);
 		MPI_Send( buf, len, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 	} else {
 		for (int i = 1; i < comsize; ++i) {
-			char buf[buf_size]; int len = 0;
-			for (uint32_t j = 0; j < buf_size; ++j) buf[j] = '\0';
-			MPI_Status status;
+			int len = 0; MPI_Status status;
 			MPI_Recv(&len,   1,  MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+			char *buf; MPI_Alloc_mem(len + 1, MPI_INFO_NULL, buf);
+			if (!buf) MPI_Abort(MPI_COMM_WORLD, 911);
 			MPI_Recv( buf, len, MPI_CHAR, i, 0, MPI_COMM_WORLD, &status);
 			gmp_sscanf(buf, "%Zd", &prod);
 			mpz_add(sum, sum, prod);
+			MPI_Free_mem(buf);
 			//gmp_printf("When I add form %d prod = %Zd sum is : %Zd\n", i, prod, sum);
 		}
 		mpz_add_ui(sum, sum, 1);
