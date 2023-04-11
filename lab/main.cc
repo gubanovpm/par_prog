131b76fe2653243a9aebabaa83f30d9db119ba6b
gubanovpm <petrgubanov37@gmail.com>
2023-04-11 12:53:15 +0300

    for swap system

--- lab/main.cc
+++ lab/main.cc
@@ -1,11 +1,11 @@
 #include "transfer_equation.hh"
 
 #ifndef N
-#define N 3
+#define N 1000
 #endif
 
 #ifndef M
-#define M 3
+#define M 1000
 #endif
 
 #ifndef T
@@ -25,14 +25,14 @@ int main() {
 	simple_conveyor(f, phi, psi, data);
 
 	auto [R, Y] = matplot::meshgrid(matplot::linspace(0, T, N), matplot::linspace(0, X, M));
-	auto [I, J] = matplot::meshgrid(matplot::linspace(0, N, N), matplot::linspace(0, M, M));
+	auto [I, J] = matplot::meshgrid(matplot::linspace(0, N-1, N), matplot::linspace(0, M-1, M));
 	auto Z = matplot::transform(I, J, [&data](size_t k, size_t m) {
         return data(k, m);
     });
 
 	matplot::surf(R, Y, Z);
 
-  matplot::show();
+  	matplot::show();
 
 	return 0;
 }
